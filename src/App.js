6e2c34b880cf52e45028b70488e9562262be40a7
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-03-09 00:18:16 +0100

    axios | step6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Filter = ({ filter, handleFilterChange }) => {
   return (
@@ -41,15 +42,19 @@ const Persons = ({ persons, filter }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
+
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+useEffect( () => {
+  axios
+  .get('http://localhost:3001/persons')
+  .then(response => {
+    setPersons(response.data)
+  })
+}, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
