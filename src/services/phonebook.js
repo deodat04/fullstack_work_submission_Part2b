3f2245e3f5c82eb7732fee06a85eb8db8097ab50
Déodat ADANDEDJAN <deodatadandedjan2000@gmail.com>
2024-03-19 17:51:33 +0100

    connect to backend

--- src/services/phonebook.js
+++ src/services/phonebook.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
+
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
